613f3d37216fb83583d1e07481b8b631557f0dca
secile develop <secile.develop@hotmail.com>
2017-09-14 00:16:42 +0900

    プロジェクト ファイルを追加します。

---
+++ OpenH264Lib/OpenH264Lib.cpp
@@ -0,0 +1,6 @@
+// これは メイン DLL ファイルです。
+
+#include "stdafx.h"
+
+#include "OpenH264Lib.h"
+
---
+++ OpenH264Lib/OpenH264Lib.h
@@ -0,0 +1,13 @@
+// OpenH264Lib.h
+
+#pragma once
+
+using namespace System;
+
+namespace OpenH264Lib {
+
+	public ref class Class1
+	{
+		// TODO: このクラスの、ユーザーのメソッドをここに追加してください。
+	};
+}
